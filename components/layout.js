32753895218389b81081b825183fef38248be700
Dhony <dhony.web@gmail.com>
2020-11-27 19:00:56 -0300

    Changing the siteTitle variable

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Dhony Silva'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Dhony Silva - My first NextJS app'
 
 export default function Layout({ children, home }) {
   return (
